2ef18a23b1cefed09889fea65dbf53a8a2047d26
Luis Sanches <luissanches@gmail.com>
2021-03-11 14:59:45 -0300

    v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,29 @@
 import Head from 'next/head'
 import { useState } from 'react'
 import useSWR from 'swr'
+import moment from 'moment'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 function Home() {
-  let [count, setCount] = useState(0) 
-  // const { data, error } = useSWR('http://localhost:3000/api/users', fetcher)
-  // if (error) return <div>failed to load</div>
-  // if (!data) return <div>loading...</div>
+  let [count, setCount] = useState(0)
+  const { data, error } = useSWR('https://nextjs-multi-tenent-boilerplate.vercel.app/api/users', fetcher)
+  let today = moment().format('DD/MM/YYYY - HH:mm:ss')
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
   return (
     <div>
       <Head>
-        <title>Create Next Yoston</title>
+        <title>Multi Tenent Application</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <h1>Next Test {data && data.name}</h1> */}
+      <h1>Next Test {data.name}</h1>
       <p>count: {count}</p>
-      <button onClick={()=>setCount(count+1)}>ok</button>
-      
+      <p>date: {today}</p>
+      <button onClick={() => setCount(count + 1)}>ok</button>
     </div>
   )
 }
-// export const getStaticProps = async () => {
-//   const data = await fetcher('http://localhost:3000/api/users')
-//   return {
-//     props:{
-//       name: data.name
-//     },
-//     revalidate: 10
-//   }
-// }
 
 
 export default Home
--- src/pages/screens/login.js
+++ src/pages/screens/login.js
@@ -7,16 +7,12 @@ import moment from 'moment'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
-function SignUp({ data, today }) {
+function Login({ data, today }) {
     let [count, setCount] = useState(0)
-    //   const { data, error } = useSWR('http://localhost:3000/api/users', fetcher)
-    //   if (error) return <div>failed to load</div>
-    //   if (!data) return <div>loading...</div>
-    // alert(JSON.stringify(data))
     return (
         <div>
             <Head>
-                <title>Multi Tenent Application [sign up]</title>
+            <title>Multi Tenent Application [login]</title>
             </Head>
             <h1>Next Test {data.name}</h1>
             <p>count: {count}</p>
@@ -26,17 +22,16 @@ function SignUp({ data, today }) {
     )
 }
 
-export const getStaticProps = async (context) => {
-    const data = await fetcher('https://nextjs-multi-tenent-boilerplate.vercel.app/api/users')
+export const getServerSideProps = async (context) => {
     let today = moment().format('DD/MM/YYYY - HH:mm:ss')
+    const data = await fetcher('https://nextjs-multi-tenent-boilerplate.vercel.app/api/users')
     return {
         props: {
             today,
             data
-        },
-        revalidate: 3
+        }
     }
 }
 
 
-export default SignUp
+export default Login
